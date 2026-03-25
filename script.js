faf06b225230beb07a346a052c2d715b437a0cce
sleepingded <torneo2x2@gmail.com>
2026-03-25 21:02:03 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+var map = L.map('map').setView([50.1109, 8.6821], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+ maxZoom: 19
+}).addTo(map);
+
+fetch("artworks.json")
+.then(response => response.json())
+.then(data => {
+
+ data.forEach(art => {
+
+  var marker = L.marker([art.lat, art.lng]).addTo(map);
+
+  marker.bindPopup(`
+   <h3>${art.name}</h3>
+   <img src="${art.image}" width="200">
+   <p>${art.description}</p>
+  `);
+
+ });
+
+});
